09b49abeeba1ae16426627daaff3213928f28b75
pmin825 <pmin.0825@gmail.com>
2021-04-19 17:28:30 -0700

    implement seperate zones on frontend

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,8 +6,13 @@ const socket = io();
 
 const Chat = () => {
   const [chatUsers, setChatUsers] = useState([]);
-  const [chatMessage, setChatMessage] = useState({ name: "", msg: "" });
+  const [chatMessage, setChatMessage] = useState({
+    name: "",
+    msg: "",
+    zone: "",
+  });
   const [msgList, setMsgList] = useState([]);
+  const [currentZone, setCurrentZone] = useState("General Lobby");
 
   useEffect(() => {
     socket.emit("updateUsers");
@@ -31,6 +36,7 @@ const Chat = () => {
     const newMessage = {
       name: chatMessage.name,
       msg: chatMessage.msg,
+      zone: currentZone,
     };
 
     socket.emit("newMessage", newMessage);
@@ -41,19 +47,47 @@ const Chat = () => {
     });
   };
 
+  const joinZone = (e) => {
+    let oldZone = currentZone;
+    let newZone = e.target.textContent;
+    setCurrentZone(newZone);
+    socket.emit("zoneJoined", { oldZone, newZone });
+    setMsgList([]);
+  };
+
   return (
     <Container>
       <Row>
         <Col xs={5} style={{ border: "1px solid black" }}>
+          <br />
+          <h6 onClick={joinZone} style={{ cursort: "pointer" }}>
+            General Lobby
+          </h6>
+          <h1>Chat Zones:</h1>
+          <ul style={{ listStyleType: "none" }}>
+            <li onClick={joinZone} style={{ cursor: "pointer" }}>
+              Tech
+            </li>
+            <li onClick={joinZone} style={{ cursor: "pointer" }}>
+              Sports
+            </li>
+            <li onClick={joinZone} style={{ cursor: "pointer" }}>
+              Food
+            </li>
+          </ul>
           <h1>Users Online:</h1>
           <ul style={{ listStyleType: "none" }}>
             {chatUsers.map((user) => {
-              return <li key={user}>{user}</li>;
+              return (
+                <li onClick={joinZone} key={user}>
+                  {user}
+                </li>
+              );
             })}
           </ul>
         </Col>
         <Col style={{ border: "1px solid black" }}>
-          <p>Chat Messages:</p>
+          <p>Chat Messages ({currentZone})</p>
           <form onSubmit={newMessageSubmit}>
             <input
               type="text"
